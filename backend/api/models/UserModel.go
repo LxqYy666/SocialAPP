d9461dbca7db879d8c86a65a5cb29bf9fd9afcbe
LxqYy666 <2334110306@qq.com>
2026-04-13 10:59:23 +0800

    创建用户模型

---
+++ backend/api/models/UserModel.go
@@ -0,0 +1,14 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Name      string             `json:"name" bson:"name"`
+	Email     string             `json:"email" bson:"email" binding:"required,email"`
+	Password  string             `json:"password" bson:"password" binding:"required,min=5"`
+	ImageUrl  string             `json:"imageUrl" bson:"imageUrl"`
+	Bio       string             `json:"bio" bson:"bio"`
+	Followers []string           `json:"followers" bson:"followers"`
+	Following []string           `json:"following" bson:"following"`
+}
